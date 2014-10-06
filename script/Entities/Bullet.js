166bed8c5e0b29648670bac7c849588bb8f1f108
Miłosz Mazur <mordimmer@gmail.com>
2014-10-06 21:11:49 +0200

    chapter 5

--- script/Entities/Bullet.js
+++ script/Entities/Bullet.js
@@ -9,6 +9,13 @@ ENGINE.Bullet = function(args) {
 
   this.radius = 3;
 
+  this.sprite = this.sprites[this.team]
+
+  this.width = this.sprite[2];
+  this.height = this.sprite[3];
+  this.radius = Math.min(this.width, this.height) / 2 | 0;
+
+
 };
 
 ENGINE.Bullet.prototype = {
@@ -18,6 +25,11 @@ ENGINE.Bullet.prototype = {
   zIndex: 3,
 
   collidable: true,
+
+  sprites: [
+    [20, 53, 6, 6],
+    [43, 53, 6, 6],
+  ],
 
   collision: function(entity) {
 
@@ -50,7 +62,14 @@ ENGINE.Bullet.prototype = {
 
   render: function() {
 
-    app.layer.fillStyle("#fff").fillRect(this.x - 4, this.y - 4, 8, 8);
+    // app.layer.fillStyle("#fff").fillRect(this.x - 4, this.y - 4, 8, 8);
+
+    app.layer.save();
+
+    app.layer.translate(this.x, this.y);
+    app.layer.drawRegion(app.images.spritesheet, this.sprite, -this.width / 2, -this.height / 2);
+    app.layer.restore();
+
 
   }
 
